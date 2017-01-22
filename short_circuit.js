95b1d3dbe3c3eeb25d620b050f363b06e05bd90d
Nicolai <nicolai@mmg.io>
2017-01-22 13:29:27 +0000

    Working with short circuit conditionals

--- short_circuit.js
+++ short_circuit.js
@@ -1,28 +1,15 @@
-var day = 0;
+// function isAdult( age ) {
+//  if (age && age >= 18) {
+//   return true;
+//  } else {
+//   return false;
+//  }
+// }
 
-switch (day) {
- case 0:
-  console.log('Sunday');
-  break;
- case 1:
-  console.log('Monday');
-  break;
- case 2:
-  console.log('Tuesday');
-  break;
- case 3:
-  console.log('Wednesday');
-  break;
- case 4:
-  console.log('Thursday');
-  break;
- case 5:
-  console.log('Friday');
-  break;
- case 6:
-  console.log('Saturday');
-  break;
- default:
-  console.log('Invalid Day');
-  break;
+// Can also be
+
+function isAdult( age ) {
+ return age && age >=18
 }
+
+console.log(isAdult(15));
